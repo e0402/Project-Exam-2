4fc1f14860a3d7bbbdd6f271ae8d1e3a86ca5a6c
e0402 <100483961+e0402@users.noreply.github.com>
2023-11-06 12:34:22 +0100

    Adds imports/Route paths in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import SingleListing from './pages/SingleListing';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/listing/:id" element={<SingleListing />} />
+        {/* ...other routes... */}
+      </Routes>
+    </Router>
   );
 }
 
