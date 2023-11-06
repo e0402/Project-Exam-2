89c134898eac9198573a0a770c93ab7d411b51eb
e0402 <100483961+e0402@users.noreply.github.com>
2023-11-06 12:25:19 +0100

    Adds ListingCard component

--- src/components/common/ListingsCard.jsx
+++ src/components/common/ListingsCard.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import RatingStars from '../RatingStars';
 
 const ListingsCard = ({ home }) => {
   return (
-    <div className="flex flex-col bg-white rounded-lg shadow-md overflow-hidden">
-      <div className="w-full h-48 bg-gray-200 flex-none">
+    <Link to={`/listing/${home.id}`} className="text-inherit no-underline">
+    <div className="flex flex-col bg-white rounded-lg shadow-md overflow-hidden h-full">
+      <div className="w-full h-48 bg-gray-200 mb-2">
         <img
-          src={home.media}
+          src={home.media[0]}
           alt={home.name}
           className="object-cover w-full h-full"
         />
       </div>
       <div className="flex flex-col flex-grow p-4">
-        <h3 className="text-lg font-semibold">{home.name}</h3>
-        <p className="flex-1 text-gray-600 mt-2">{home.description}</p>
+        <RatingStars rating={home.rating} />
+        <h3 className="text-lg font-semibold mt-5">{home.name}</h3>
+        <p className="flex-grow text-gray-600 mt-2 mb-5 line-clamp-3">{home.description}</p>
       </div>
     </div>
+    </Link>
   );
 };
 
