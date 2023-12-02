89ac5b1797f616e1be24689db235f1adbc9a7f7e
e0402 <100483961+e0402@users.noreply.github.com>
2023-12-02 01:54:41 +0100

    Adds create venue page component

---
+++ src/components/pages/CreateVenuePage/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import CreateVenueForm from '../../forms/CreateVenueForm';
+
+const CreateVenuePage = () => {
+  return (
+    <div className="container mx-auto px-4 py-8 max-w-7xl">
+      <h1 className="text-4xl font-bold text-center mb-12 mt-12">Create venue listing</h1>
+      <div className='p-4 max-w-3xl mx-auto'>
+        <p className='mb-5'>Please provide as much detail as possible when registering your venue. This both increases the likelyhood of bookings and prevents unnecesairy questions by potential customers.</p>
+        <p className='font-semibold mb-5'>Fields marked with * are required. The rest is optional, although we recommend using all of them</p>
+      </div>
+      <CreateVenueForm />
+    </div>
+  );
+};
+
+export default CreateVenuePage;
