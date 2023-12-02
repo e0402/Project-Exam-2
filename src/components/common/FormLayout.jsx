e4be42204aa05ab9ab9d51711df794b53aa4979c
e0402 <100483961+e0402@users.noreply.github.com>
2023-12-02 01:57:30 +0100

    Deleted components

--- src/components/common/FormLayout.jsx
+++
@@ -1,9 +0,0 @@
-export const FormLayout = ({ children, onSubmit }) => (
-    <form onSubmit={onSubmit} className="max-w-xl mx-auto p-4">
-      {children}
-      <button type="submit" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700">
-        Submit
-      </button>
-    </form>
-  );
-  
--- src/components/common/Input.jsx
+++
@@ -1,8 +0,0 @@
-export const Input = ({ register, name, label, error, ...rest }) => (
-    <div className="mb-4">
-      <label htmlFor={name} className="block text-sm font-medium text-gray-700">{label}</label>
-      <input id={name} {...register(name)} className="mt-1 block w-full border-2 border-gray-300 p-2 rounded-md shadow-sm" {...rest} />
-      {error && <p className="mt-2 text-sm text-red-600">{error}</p>}
-    </div>
-  );
-  
