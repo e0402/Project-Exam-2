08e727ebadf38448035e16e393e05163a5364dd4
e0402 <100483961+e0402@users.noreply.github.com>
2023-12-02 02:00:54 +0100

    Adds custom tailwind rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        13: "3.15rem",
+      },
+      maxWidth: {
+        xxs: "13.5rem",
+      },
+      backgroundColor: {
+        "off-white": "rgb(253, 253, 253)",
+      },
+    },
   },
   plugins: [],
 };
