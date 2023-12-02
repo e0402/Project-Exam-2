8ca306f9deea009b87fb4bf4ec2883aec57459b2
e0402 <100483961+e0402@users.noreply.github.com>
2023-12-02 02:04:11 +0100

    Changes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import SingleVenue from './pages/SingleVenuePage';
-import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LoginPage';
+import Layout from './components/common/Layout';
+import HomePage from './components/pages/HomePage';
+import SingleVenue from './components/pages/SingleVenuePage';
+import RegisterPage from './components/pages/RegisterPage';
+import LoginPage from './components/pages/LoginPage';
+import ProfilePage from './components/pages/ProfilePage';
+import MyBookingsPage from './components/pages/MyBookingsPage';
+import MyListingsPage from './components/pages/MyListingsPage';
+import CreateVenuePage from './components/pages/CreateVenuePage'
+import EditVenuePage from './components/pages/EditVenuePage';
+import { ModalProvider } from './components/common/ModalContext';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    setIsLoggedIn(!!localStorage.getItem('authToken'));
+  }, []);
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/venue/:id" element={<SingleVenue />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
+      <ModalProvider>
+        <Layout isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/venue/:id" element={<SingleVenue />} />
+            <Route path="/login" element={<LoginPage setIsLoggedIn={setIsLoggedIn}/>} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/my-bookings" element={<MyBookingsPage />} />
+            <Route path="/create-venue" element={<CreateVenuePage />} />
+            <Route path="/my-listings" element={<MyListingsPage />} />
+            <Route path="/edit-venue/:venueId" element={<EditVenuePage />} />
+          </Routes>
+        </Layout>
+      </ModalProvider>
     </Router>
   );
 }
