bce6e79a1df5e08a6018ee00e487f29618a3a95c
e0402 <100483961+e0402@users.noreply.github.com>
2023-12-02 01:41:38 +0100

    Changes to nav/footer components

---
+++ src/components/common/Footer/index.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+
+function Footer() {
+  return (
+    <footer className="border border-gray-300 text-black py-8 mt-12">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+
+          <div>
+            <h3 className="font-bold text-xl mb-4">Resources</h3>
+            <ul className="list-none space-y-3">
+              <li><span className="hover:text-gray-300 cursor-pointer">FAQ</span></li>
+              <li><span className="hover:text-gray-300 cursor-pointer">Policy</span></li>
+              <li><span className="hover:text-gray-300 cursor-pointer">Career</span></li>
+            </ul>
+          </div>
+
+          <div>
+            <h3 className="font-bold text-xl mb-4">Company</h3>
+            <ul className="list-none space-y-3">
+              <li><span className="hover:text-gray-300 cursor-pointer">About Us</span></li>
+              <li><Link to="/contact" className="hover:text-gray-300">Contact</Link></li>
+              <li><span className="hover:text-gray-300 cursor-pointer">Terms Of Use</span></li>
+            </ul>
+          </div>
+
+          <div>
+            <h3 className="font-bold text-xl mb-4">Follow Us</h3>
+            <ul className="list-none space-y-3">
+              <li>
+                <span className="hover:text-gray-300 flex items-center cursor-pointer">
+                  <FaFacebookF className="mr-2" /> Facebook
+                </span>
+              </li>
+              <li>
+                <span className="hover:text-gray-300 flex items-center cursor-pointer">
+                  <FaTwitter className="mr-2" /> Twitter
+                </span>
+              </li>
+              <li>
+                <span className="hover:text-gray-300 flex items-center cursor-pointer">
+                  <FaInstagram className="mr-2" /> Instagram
+                </span>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h4 className="font-bold text-xl mb-4">Newsletter</h4>
+            <input 
+              type="email" 
+              className="mt-3 p-2 rounded w-full border border-gray-500" 
+              placeholder="example123@something.com" 
+            />
+            <button className="bg-white hover:bg-gray-100 text-blue-700 border border-blue-700 font-semibold py-2 px-7 rounded mt-2 w-full">Subscribe</button>
+          </div>
+
+        </div>
+        <div className="mt-10 border-t border-gray-700 pt-5 flex justify-center">
+            <p>&copy; {new Date().getFullYear()} Holidaze. All Rights Reserved.</p>
+        </div>
+
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/common/Navbar/index.jsx
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { FaHome, FaBars, FaTimes } from 'react-icons/fa';
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('authToken'));
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleLoginSuccess = () => {
+      setIsLoggedIn(true);
+    };
+
+    window.addEventListener('login-success', handleLoginSuccess);
+
+    return () => {
+      window.removeEventListener('login-success', handleLoginSuccess);
+    };
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('isVenueManager');
+    localStorage.removeItem('username');
+    setIsLoggedIn(false);
+    navigate('/login');
+  };
+
+  return (
+    <nav className="border border-gray-300 text-black sticky top-0 bg-off-white z-10">
+      <div className="mx-2 md:mx-9 p-2">
+        <div className="flex justify-between items-center">
+          {/* Logo */}
+          <NavLink to="/" className="flex items-center py-3 text-black hover:text-gray-800">
+            <FaHome className="h-7 w-7 mr-2" />
+            <span className="text-3xl font-bold">Holidaze</span>
+          </NavLink>
+
+          {/* Primary Nav */}
+          <div className="hidden md:flex items-center text-sm">
+            {isLoggedIn && (
+            <NavLink to="/profile" className="py-3 px-4 text-base font-semibold hover:text-gray-200">Profile</NavLink>
+            )}
+              {!isLoggedIn && (
+              <NavLink to="/register" className="bg-white hover:bg-gray-100 text-blue-700 border border-blue-700 font-semibold py-2 px-7 rounded-full mr-3">Sign Up</NavLink>
+            )}
+            {isLoggedIn ? (
+              <button onClick={handleLogout} className="bg-blue-600 hover:bg-blue-700 text-white border border-blue-700 font-semibold py-2 px-8 rounded-full">Log out</button>
+            ) : (
+              <NavLink to="/login" className="bg-blue-600 hover:bg-blue-700 text-white border border-blue-700 font-semibold py-2 px-8 rounded-full">Login</NavLink>
+            )}
+          </div>
+
+          {/* Mobile Button */}
+          <div className="md:hidden">
+            <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+              {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+            </button>
+          </div>
+        </div>
+
+        {/* Mobile Menu */}
+        <div className={`${isMenuOpen ? 'block' : 'hidden'} md:hidden`}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "block py-2 px-3 text-sm font-semibold bg-gray-300" : "block py-2 px-3 text-sm font-semibold hover:text-gray-600"}>Home</NavLink>
+
+          {isLoggedIn && (
+            <NavLink
+              to="/profile"
+              className={({ isActive }) =>
+                isActive ? "block py-2 px-3 text-sm font-semibold bg-gray-300" : "block py-2 px-3 text-sm font-semibold hover:text-gray-600"
+              }>Profile
+            </NavLink>
+          )}
+
+          {isLoggedIn ? (
+            <button onClick={handleLogout} className="block w-full text-left py-2 px-3 text-sm font-semibold hover:text-gray-600">
+              Logout
+            </button>
+          ) : (
+            <NavLink
+              to="/login"
+              className={({ isActive }) =>
+                isActive ? "block py-2 px-3 text-sm font-semibold bg-gray-300" : "block py-2 px-3 text-sm font-semibold hover:text-gray-600"
+              }>Login
+            </NavLink>
+          )}
+
+          {!isLoggedIn && (
+            <NavLink
+              to="/register"
+              className={({ isActive }) =>
+                isActive ? "block py-2 px-3 text-sm font-semibold bg-gray-300" : "block py-2 px-3 text-sm font-semibold hover:text-gray-600"
+              }>Sign Up
+            </NavLink>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
