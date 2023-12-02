09320b84bb477db25afd711f4a8ca9a1442463f8
e0402 <100483961+e0402@users.noreply.github.com>
2023-12-02 03:25:34 +0100

    Minor text changes

--- src/components/pages/ProfilePage/index.jsx
+++ src/components/pages/ProfilePage/index.jsx
@@ -4,7 +4,7 @@ import ProfileForm from '../../forms/ProfileForm';
 const ProfilePage = () => {
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold text-center mb-12 mt-12">Personal details</h1>
+      <h1 className="text-4xl font-bold text-center mb-12 mt-12">Personal Details</h1>
       <ProfileForm />
     </div>
   );
