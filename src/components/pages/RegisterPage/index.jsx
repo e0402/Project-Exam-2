a9c225911494d6b33aa839a7da58a2173ee4296e
e0402 <100483961+e0402@users.noreply.github.com>
2023-12-02 03:23:57 +0100

    Adds placeholder text inputs

--- src/components/pages/RegisterPage/index.jsx
+++ src/components/pages/RegisterPage/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import RegisterForm from '../../forms/RegisterForm';
+import { FaUserPlus } from 'react-icons/fa';
 
 const RegisterPage = () => {
   return (
     <div className="container mx-auto px-4 py-8">
-      <h2 className="text-2xl font-bold text-center mb-4">Register</h2>
+      <h2 className="text-2xl font-bold text-center mb-4">Register <FaUserPlus className="inline-block ml-2 text-2xl relative top-[-2px]" /></h2>
       <RegisterForm />
     </div>
   );
