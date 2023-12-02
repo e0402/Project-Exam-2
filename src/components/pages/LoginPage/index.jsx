dcec17be068887d5f73cd964d1704a0b4c61f7b2
e0402 <100483961+e0402@users.noreply.github.com>
2023-12-02 03:33:34 +0100

    Top margin auth pages

--- src/components/pages/LoginPage/index.jsx
+++ src/components/pages/LoginPage/index.jsx
@@ -4,7 +4,7 @@ import { FaSignInAlt } from 'react-icons/fa';
 
 const LoginPage = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 mt-12">
       <h1 className="text-2xl font-bold text-center mb-4">Login <FaSignInAlt className="inline-block ml-2 text-2xl relative top-[-2px]" /></h1>
       <LoginForm isModal={false} />
     </div>
--- src/components/pages/RegisterPage/index.jsx
+++ src/components/pages/RegisterPage/index.jsx
@@ -4,7 +4,7 @@ import { FaUserPlus } from 'react-icons/fa';
 
 const RegisterPage = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 mt-12">
       <h2 className="text-2xl font-bold text-center mb-4">Register <FaUserPlus className="inline-block ml-2 text-2xl relative top-[-2px]" /></h2>
       <RegisterForm />
     </div>
